e6876590a40b2368402b7749c594fe3f88161de6
Nickolasy Yurin <nyurin@griddynamics.com>
2013-08-23 16:14:50 +0400

    Added custom ArgumentErrors for bucket and folder nonexistence

--- cookbooks/s3manage/definitions/manage.rb
+++ cookbooks/s3manage/definitions/manage.rb
@@ -4,12 +4,15 @@ define :s3manage, :bucket => nil, :s3file => nil, :action => nil do
     raise ArgumentError, "Unsupported action: #{params[:action]}"
   end
 
+  unless ::File.exists?("#{params[:name]}")
+    raise ArgumentError, "No such file or directory: #{params[:name]}"    
+  end
+
   case params[:action]
     when "upload"    
       unless ::File.exists?("#{params[:name]}")
         raise ArgumentError, "No such file: #{params[:name]}"
       end
-
       ruby_block "s3upload" do
         block do
           require 'rubygems'
@@ -22,6 +25,10 @@ define :s3manage, :bucket => nil, :s3file => nil, :action => nil do
 
           base_name = File.basename("#{params[:name]}")
 
+          unless s3.buckets["#{bucket}"].exists?
+            raise ArgumentError, "No such bucket: #{params[:bucket]}"
+          end
+          
           bucket = s3.buckets["#{params[:bucket]}"]
           file = bucket.objects["#{base_name}"]
           file.write(:file => params[:name])
@@ -34,7 +41,6 @@ define :s3manage, :bucket => nil, :s3file => nil, :action => nil do
           action :create
           recursive true
         end
-        
       end
       ruby_block "s3download" do
         block do
@@ -48,10 +54,18 @@ define :s3manage, :bucket => nil, :s3file => nil, :action => nil do
 
           base_name = File.basename("#{params[:name]}")
 
+          unless s3.buckets["#{bucket}"].exists?
+            raise ArgumentError, "No such bucket: #{params[:bucket]}"
+          end
+
+          unless bucket.objects["#{params[:s3file]}"].exists?
+            raise ArgumentError, "No such file on #{params[:bucket]} bucket: #{params[:s3file]}"
+          end
+
           bucket = s3.buckets["#{params[:bucket]}"]
           file = bucket.objects["#{params[:s3file]}"]     
 
-          File.open("#{params[:name]}/#{params[:s3file]}", 'wb') do |local_file|
+          File.open("#{params[:name]}/#{File.basename(params[:s3file])}", 'wb') do |local_file|
             file.read do |chunk|
                local_file.write(chunk)
             end

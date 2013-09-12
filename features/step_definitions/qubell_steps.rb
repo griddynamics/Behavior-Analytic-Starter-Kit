f52aabb27121baa88a0f9cde33694fdd7abd07d4
Denis Khurtin <dkhurtin@griddynamics.com>
2013-09-12 14:42:58 +0400

    Fix cucumber tests

--- features/step_definitions/qubell_steps.rb
+++ features/step_definitions/qubell_steps.rb
@@ -3,9 +3,15 @@ Given(/^I have a qubell platform$/) do
   @qubell = QubellPlatform.new
 end
 
+Given(/^I have a instance of '(.*)'$/) do |app_name|
+  @app_instance = $qubell_instances[app_name]
+  raise 'Instance should be existed.' if @app_instance.nil?
+end
+
 When(/^I start new instance of '(.*)'$/) do |app_name|
-  @app = @qubell.create_application app_name
-  @app_instance = @qubell.launch_application @app
+  app = @qubell.create_application app_name
+  @app_instance = @qubell.launch_application app
+  $qubell_instances[app_name] = @app_instance
 end
 
 When(/^I run workflow '(.*)'$/) do |workflow_name|
@@ -13,14 +19,5 @@ When(/^I run workflow '(.*)'$/) do |workflow_name|
 end
 
 Then(/^I wait when instance status will changed from '(.*)' to '(.*)'$/) do |from_status, to_status|
-  instance_info = @qubell.fetch_instance_info @app_instance
-
-  while instance_info['status'] == from_status
-    sleep 5
-    instance_info = @qubell.fetch_instance_info @app_instance
-  end
-
-  if instance_info['status'] != to_status
-    raise "Instance has #{instance_info['status']} status, expected: #{to_status}"
-  end
+  @app_instance.wait_status from_status, to_status
 end
---
+++ features/support/env.rb
@@ -0,0 +1,18 @@
+require 'logger'
+
+$logger = Logger.new(STDERR)
+$logger.level = Logger::DEBUG
+$logger.formatter = proc do |severity, datetime, progname, msg|
+  "[#{severity}] (#{datetime.strftime('%Y-%m-%d %H:%M:%S')}) #{msg}\n"
+end
+
+$qubell_instances = {}
+
+at_exit do
+  $logger.info 'Destroying all instances:' unless $qubell_instances.empty?
+
+  $qubell_instances.values.each do |instance|
+    $logger.info "Destroying #{instance.get_id}"
+    instance.destroy
+  end
+end
